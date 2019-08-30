4d88e8443ac7855fe9d2b3244f43d6a27f9335fe
Daxo <17711952+Daxo@users.noreply.github.com>
2019-08-30 11:34:38 -0500

    refactored Map to a functional component

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,65 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
 
 import MarkerPin from './MarkerPin';
 
 import LineOverlay from './LineOverlay';
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewport: {
-        width: 800,
-        height: 400,
-        longitude: -122.39,
-        latitude: 37.7128,
-        zoom: 8
-      },
-      loading: true
-    };
-  }
+export default function Map(props) {
+  const { locations, addLocation, removeAllLocations } = props;
 
-  componentDidMount() {
-    this.setState({ loading: false });
-  }
-
-  handleViewportChange = viewport => {
-    const { loading } = this.state;
-    if (!loading) {
-      this.setState({ viewport });
-    }
+  const startingViewport = {
+    width: 800,
+    height: 400,
+    longitude: -122.39,
+    latitude: 37.7128,
+    zoom: 8
   };
 
-  render() {
-    const { viewport } = this.state;
-    const { locations, addLocation, removeAllLocations } = this.props;
+  const [viewport, setViewport] = useState(startingViewport);
 
-    return (
-      <div data-testid='map'>
-        <ReactMapGL
-          {...viewport}
-          onViewportChange={this.handleViewportChange}
-          onClick={addLocation}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        >
-          {locations.map((m, i) => (
-            <Marker {...m} key={i}>
-              <MarkerPin first={i === 0} />
-            </Marker>
-          ))}
-          <LineOverlay
-            locations={locations}
-            compositeOperation='lighter'
-            renderWhileDragging={true}
-          />
-        </ReactMapGL>
-        <button className='btn btn-secondary m-2' onClick={removeAllLocations}>
-          Remove Markers
-        </button>
-      </div>
-    );
-  }
+  const handleViewportChange = viewport => {
+    setViewport({ ...viewport });
+  };
+
+  return (
+    <div data-testid='map'>
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={handleViewportChange}
+        onClick={addLocation}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      >
+        {locations.map((m, i) => (
+          <Marker {...m} key={i}>
+            <MarkerPin first={i === 0} />
+          </Marker>
+        ))}
+        <LineOverlay
+          locations={locations}
+          compositeOperation='lighter'
+          renderWhileDragging={true}
+        />
+      </ReactMapGL>
+      <button className='btn btn-secondary m-2' onClick={removeAllLocations}>
+        Remove Markers
+      </button>
+    </div>
+  );
 }
-
-export default Map;
