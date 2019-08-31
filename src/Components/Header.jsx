0d1ab1e39f180772bc815b1e4757f6a8923bdc3c
Daxo <17711952+Daxo@users.noreply.github.com>
2019-08-31 13:52:44 -0500

    Updated favicon and Readme

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,12 @@ export default function Header() {
   return (
     <>
       <Full>
-        <h1 className='text-center'>Mapper</h1>
+        <h1
+          className='text-center'
+          style={{ fontFamily: 'Leckerli One, cursive', fontSize: '4.25em' }}
+        >
+          Mapper
+        </h1>
       </Full>
       <p className='text-center'>A map-based travel planner</p>
     </>
